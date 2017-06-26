977786cef4a0fe5190b002f30d0b72d0c8cff7ce
Sabbertran <info@sabbertran.de>
2017-06-26 18:55:34 +0200

    Added UTF-8 support (experimental)

--- src/main/java/de/sabbertran/proxysuite/handlers/AnnouncementHandler.java
+++ src/main/java/de/sabbertran/proxysuite/handlers/AnnouncementHandler.java
@@ -24,7 +24,8 @@ public class AnnouncementHandler {
         announcements = new ArrayList<String>();
         File f = new File(main.getDataFolder(), "announcements.yml");
         try {
-            BufferedReader read = new BufferedReader(new FileReader(f));
+            //BufferedReader read = new BufferedReader(new FileReader(f));
+            BufferedReader read = new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF8"));
             String line;
             while ((line = read.readLine()) != null) {
                 line = line.trim();
